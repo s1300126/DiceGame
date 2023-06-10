aa452424a81e641813d2e2a35773631b7ea11071
s1300126 <s1300126@u-aizu.ac.jp>
2023-06-10 21:43:00 +0900

    Won/lost message added

--- DiceGameApplication.java
+++ DiceGameApplication.java
@@ -12,6 +12,7 @@ class DiceGameApplication{
             System.out.println("Die " + (i+1) + ": " + a);
         }
         System.out.println("Total value: " + total);
-
+        if(total > 7)System.out.println("You won!");
+        else System.out.println("You lost!");
     }
 }
